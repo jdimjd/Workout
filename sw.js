b19c538561f70b3728f157c4eb24e30e4d1d03cb
jdimjd <jh62709@gmail.com>
2026-09-11 16:36:56 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+// Purana fas chuka cache clear karega aur offline issue resolve karega
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((k) => caches.delete(k)));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
+});
